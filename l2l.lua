1c315b4d7443422655cfcff7afe17312a8b489a9
Eric Man <meric.au@gmail.com>
2012-01-27 22:06:50 +1100

    Complete set of minimally required primitives; Add comment parsing
    Now can compile http://lib.store.yahoo.net/lib/paulgraham/jmc.lisp (and
    run compiled lua code too).
    Added commenting to parser. Had to remove comments to keep line count
    below 100.

--- l2l.lua
+++ l2l.lua
@@ -1,40 +1,39 @@
--- input -> lisp file to be compiled
--- output -> where compiled lua code is saved
 local input, output = arg[1] or "test.lsp", arg[2] or "out.lua"
 setmt, tostr, getmt = setmetatable, tostring, getmetatable
--- list type
-list_mt = {__tostring=function(self)return "list("..sep(self, quote)..")" end}
--- list constructor
+list_mt = {__tostring=function(self)return "list("..sep(self, quote)..")" end,
+           __eq=function(self,other)
+            if car(self) ~= car(other) then return false end
+            if cdr(self) == nil and cdr(other) == nil then return true end
+            if cdr(self) == nil or cdr(other) == nil then return false end
+            return cdr(self) == cdr(other)
+           end}
 function list(v, ...) return setmt({v, ... and list(...) or nil}, list_mt) end
--- desolve list into n-tuple
 function unlist(l, f) if l then return f(car(l)), unlist(cdr(l), f) end end
--- stringify list into form "{a},{b},{c},..."
 function sep(l, f) return table.concat({unlist(l, f)}, ",") end
--- symbol type (runtime symbol)
 sym_mt = {__tostring = function(self) return self.n end}
--- operator type (modifys parse tree at compile time)
 kw_mt = {__call = function(self,...) return self.f(...)end,
          __tostring = sym_mt.__tostring}
--- function type (built-in functions)
 fun_mt = {__tostring = sym_mt.__tostring, __call = kw_mt.__call}
--- operator constructor
 function kw(n,f) return setmt({n=n,f=f}, kw_mt) end
--- symbol constructor
 function sym(n) return setmt({n=n}, sym_mt) end
--- built-in function constructor
 function fun(n,f) return setmt({n=n,f=f}, fun_mt) end
--- primitives
-_G["="] = kw("=", function(a, b) return tolua(a).."=="..tolua(b) end)
-_G["+"] = kw("+", function(a, b) return tolua(a).."+"..tolua(b) end)
-_G["-"] = kw("-", function(a, b) return tolua(a).."-"..tolua(b) end)
-_G["*"] = kw("*", function(a, b) return tolua(a).."*"..tolua(b) end)
-_G["/"] = kw("/", function(a, b) return tolua(a).."/"..tolua(b) end)
+eq = kw("eq", function(a, b) return tolua(a).."=="..tolua(b) end)
+atom = fun("atom", function(a) return getmetatable(a)~=list_mt end)
 car = fun("car", function(l) return l[1] end)
 cdr = fun("cdr", function(l) return l[2] end)
 cons = fun("cons", function(a, b) return setmt({a, b}, list_mt) end)
-label = kw("label", function (k, v) return tostring(k).."="..tolua(v) end)
+cond = kw("cond", function (...)
+  local c, n = "(function() if true then ", #{...}
+  for i, v in ipairs({...}) do
+    if n~=i then 
+      c = c.."elseif "..tolua(car(v)).." then return "..tolua(car(cdr(v))).." "
+    else c = c .. "else return "..tolua(v).." end end)()" end
+  end
+  return c
+end)
+defun = kw("defun",function(n, a, ...)
+  return "_G[\""..tostring(n).."\"]="..lambda(a, ...) end)
 print = fun("print", print)
--- quote operator
 quote = kw("quote", function(l)
   if type(l) == "string" then return "[["..l.."]]"
   elseif type(l) == "number" then return tostring(l)
@@ -43,11 +42,11 @@ quote = kw("quote", function(l)
   elseif getmt(l) == kw_mt then return "sym(\""..tostring(l).."\")"
   elseif getmt(l) == list_mt then return "list("..sep(l, quote)..")" end
 end)
--- convert parse tree to lua code
 tolua = fun("tolua", function(l)
   if type(l) == "string" then return "[["..l.."]]"
   elseif type(l) == "number" then return tostring(l)
-  elseif getmt(l) == sym_mt or getmt(l) == fun_mt then return tostring(l)
+  elseif getmt(l) == sym_mt or getmt(l) == fun_mt then 
+    return "_G[\""..tostring(l) .. "\"]"
   elseif getmt(l) == list_mt then 
     if not car(l) then return nil end
     if getmt(car(l)) == sym_mt and getmt(_G[tostring(car(l))]) == kw_mt then
@@ -58,25 +57,27 @@ tolua = fun("tolua", function(l)
     return tolua(car(l)) .."("..sep(cdr(l),tolua)..")" end
   return ""
 end)
--- lambda operator
 lambda = kw("lambda", function(args, ...) 
   local def, r = "function("..sep(args, tostring)..") ", "return "
   for i,v in ipairs({...}) do def=def..(i==#{...}and r or"")..tolua(v).." " end
   return def.."end" end)
--- compile parse tree(s)
 function compile(s,...) return tolua(s).."\n"..(... and compile(...) or "") end
 function run(src)
   local b=1 return load(function() if b then b=nil return src end end)()
 end
--- lisp eval primitive
 eval = fun("eval", function(l) return run("return "..tolua(l)) end)
 trim = function(s) return (s:gsub("^%s*(.-)%s*$", "%1")) end
--- parser
 parse = function(src)
   src = trim(src)
   local open, close = src:find("%b()")
   if open == 1 then 
     return list(parse(src:sub(2, close-1))), parse(src:sub(close+1))
+  elseif src:sub(1, 1) == ";" then
+    local rest = src:sub(2):match("%s*.-\n(.*)")
+    if rest then return parse(rest) end
+  elseif src:sub(1, 1) == "'" then
+    local rest = src:sub(2):match("%s*(.*)")
+    return list(quote, parse(rest))
   elseif src:sub(1, 1) == "\"" then
     local esc, i = false, 2
     while esc == true or src:sub(i, i)~="\"" do
@@ -92,9 +93,6 @@ parse = function(src)
 end
 if not input or not output then return end
 inf=io.open(input, "r") src = inf:read("*all") inf:close()
-of=io.open(output, "w")
-lf=io.open("l2l.lua", "r")
-  for i=1,92 do of:write(lf:read("*line").."\n") end 
-lf:close()
-of:write(compile(parse(src)).."\n")
-of:close()
+of=io.open(output, "w") lf=io.open("l2l.lua", "r")
+  for i=1,93 do of:write(lf:read("*line").."\n") end lf:close()
+of:write(compile(parse(src)).."\n") of:close()
