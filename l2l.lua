c4c9fca590f30f12f721a9ecd7ffa59ffc55ef8b
Eric Man <meric.au@gmail.com>
2012-04-04 01:00:48 +1000

    Implement quasiquote, quasiquote-eval. Needed to delete some comments to save lines.

--- l2l.lua
+++ l2l.lua
@@ -1,37 +1,37 @@
 local input, output = arg[1] or "test.lsp", arg[2] or "out.lua"
 setmt, tostr, getmt = setmetatable, tostring, getmetatable
 function id(a)return a end 
-
--- list
+function map(f, t) local m={} for i,v in ipairs(t) do m[i]=f(v) end return m end
 list_mt = {__tostring=function(self)return "list("..sep(self,quote)..")" end
-          ,__eq=function(s,o) return s[1]==o[1] and s[2]==o[2] end}
-function list(v,...)return setmt({v,...~=nil and list(...) or nil},list_mt) end
+          ,__eq=function(s,o) return s[1]==o[1] and s[2]==o[2] end
+          ,__ipairs=function(s) return function() 
+            if s then local i=s[1] s=s[2] return i or s,i end end end}
+function list(v,...)return setmt({v,next({...})and list(...) or nil},list_mt)end
 function unlist(l,f)f=f or id if l then return f(l[1]), unlist(l[2], f) end end
-function sep(l, f) return table.concat({unlist(l, f)}, ",") end
-
--- symbol & operator
+function sep(l, f) return table.concat(map(tostr, {unlist(l, f)}), ",") end
 sym_mt = {__tostring = function(self) return self.n end}
 function sym(n) return setmt({n=n}, sym_mt) end
-function hash(v)return tostr(v):gsub("%W",function(a) 
+function hash(v) return tostr(v):gsub("%W",function(a) 
   if a ~= "." then return "_c"..a:byte().."_" else return "." end end) end
 op_mt = {__call = function(self,...) return self.f(...)end}
 function op(f) return setmt({f=f}, op_mt) end
-
--- parser & compiler
 function trim(s) return (s:gsub("^%s*(.-)%s*$", "%1")) end
-function parse(src)
-  src = trim(src)
+function substitute(src, s, rp) 
+  local rest = list(parse(src:sub(#s+1):match("%s*(.*)"))) 
+  return list(sym(rp), rest[1]), unlist(rest[2])
+end
+function prefixed(src, pr) return src:sub(1, #pr) == pr end
+function parse(src) -- parse into tree
+  src = trim(src) 
   local open, close = src:find("%b()")
   if open == 1 then 
     return list(parse(src:sub(2, close-1))), parse(src:sub(close+1))
-  elseif src:sub(1, 1) == ";" then
-    local rest = src:sub(2):match("%s*.-\n(.*)")
-    if rest then return parse(rest) end
-  elseif src:sub(1, 1) == "'" then
-    local rest = src:sub(2):match("%s*(.*)")
-    local r = list(parse(rest))
-    return list(sym("quote"), r[1]), unlist(r[2])
-  elseif src:sub(1, 1) == "\"" then
+  elseif prefixed(src, ";") then
+    local r = src:sub(2):match("%s*.-\n(.*)") if r then return parse(r) end
+  elseif prefixed(src, "'") then return substitute(src, "'", "quote")
+  elseif prefixed(src, "`") then return substitute(src, "`", "quasiquote")
+  elseif prefixed(src, ",") then return substitute(src, ",", "quasiquote-eval")
+  elseif prefixed(src, "\"") then
     local esc, i = false, 2
     while esc == true or src:sub(i, i)~="\"" do
       if esc then esc = false end
@@ -39,62 +39,62 @@ function parse(src)
     end 
     return src:sub(2, i-1), parse(src:sub(i+1))
   elseif #src > 0 then
-    local first = src:match("(%S+)")
-    local rest = src:match("%S+%s+(.*)")
-    return tonumber(first) or sym(first), parse(rest or "")
-  end
+    local first, rest = src:match("(%S+)"), src:match("%S+%s+(.*)")
+    return tonumber(first) or sym(first), parse(rest or "") end
 end
-function tolua(l) -- l is parse tree
-  if type(l) == "string" then return "[["..l.."]]"
+function escape(str) return '"'..str:gsub('"','\\"')..'"' end
+function lua(l) -- l is parse tree, convert to lua
+  if type(l) == "string" then return escape(l)
   elseif type(l) == "number" then return tostring(l)
-  elseif getmt(l) == sym_mt then return hash(tostring(l))
+  elseif getmt(l) == sym_mt then return hash(l)
   elseif getmt(l) == list_mt then
     local fst = l[1]; if not fst then return nil end
     if getmt(fst) == sym_mt and getmt(_G[hash(tostring(fst))]) == op_mt then
-      return _G[hash(tostring(fst))](unlist(l[2], id))
+      return _G[hash(fst)](unlist(l[2], id))
     elseif getmt(fst) == sym_mt then
-      return hash(tostring(fst)) .."("..sep(l[2],tolua)..")"
+      return hash(fst).."("..sep(l[2],lua)..")"
     end
-    return tolua(fst).."("..sep(l[2],tolua)..")" 
-  end
+    return lua(fst).."("..sep(l[2],lua)..")" end
 end
-function compile(ret, s,...) 
-  local c = tolua(s)..(ret and " " or "\n") 
+function compile(ret, s, ...) -- convert multiple trees into lua
+  local c = "\n"..lua(s)
   if ... then return c .. compile(ret, ...) end
-  if ret then return "return ".. c else return c end
+  if ret then return "\nreturn ".. c:sub(2) else return c end
 end
-
--- primitives
-_G[hash("*")] = op(function(a, b) return "("..tolua(a).."*"..tolua(b)..")" end)
-_G[hash("/")] = op(function(a, b) return "("..tolua(a).."/"..tolua(b)..")" end)
-_G[hash("+")] = op(function(a, b) return "("..tolua(a).."+"..tolua(b)..")" end)
-_G[hash("-")] = op(function(a, b) return "("..tolua(a).."-"..tolua(b)..")" end)
-cons = op(function(a, b)return"setmt({"..sep(list(a,b),tolua).."},list_mt)"end)
-atom = op(function(a) return "(getmt("..tolua(a)..")~=list_mt)" end)
-car = op(function(a) return tolua(a).."[1]" end)
-cdr = op(function(a) return tolua(a).."[2]" end)
-eq = op(function(a, b) return tolua(a) .. "==" .. tolua(b) end)
-defun = op(function(n,a,...) return hash(tostr(n)).."="..lambda(a, ...)end)
-lambda = op(function(a, ...) 
-  local def, r = "function("..(a[1] and sep(a, hash) or "")..") ", "return "
-  for i,v in ipairs({...}) do def=def..(i==#{...}and r or"")..tolua(v).." " end
-  return def.."end" end)
+function indent(src) return src:gsub("\n", "\n  ") end -- primitives
+_G[hash("*")] = op(function(a, b) return "("..lua(a).."*"..lua(b)..")" end)
+_G[hash("/")] = op(function(a, b) return "("..lua(a).."/"..lua(b)..")" end)
+_G[hash("+")] = op(function(a, b) return "("..lua(a).."+"..lua(b)..")" end)
+_G[hash("-")] = op(function(a, b) return "("..lua(a).."-"..lua(b)..")" end)
+cons = op(function(a, b)return"setmt({"..sep(list(a,b),lua).."},list_mt)"end)
+atom = op(function(a) return "(getmt("..lua(a)..")~=list_mt)" end)
+car = op(function(a) return lua(a).."[1]" end)
+cdr = op(function(a) return lua(a).."[2]" end)
+eq = op(function(a, b) return lua(a) .. "==" .. lua(b) end)
+set = op(function(s, v) return "local ".. hash(s) .." = "..lua(v) end)
+defun = op(function(n,a,...) return "local function "..hash(n).."("..(a[1] and 
+  sep(a, hash) or "")..")"..indent(compile(true, ...)).."\nend" end)
+lambda = op(function(a, ...) return "function("..(a[1] and sep(a, hash) or "")..
+  ")"..indent(compile(true, ...)).."\nend" end)
 cond = op(function(...)
-  local def, r = "(function()", "return "
-  for i,v in ipairs({...}) do def=def.."\n  if "..tolua(v[1]).. " then ".. 
-    compile(true, unlist(v[2])) .. "end" end
-  return def.."\n  end)()" end)
+  local def = map(function(v) return "if "..lua(v[1]).." then"..
+    indent(compile(true, unlist(v[2]))).."\nend" end, {...})
+  return "(function()"..indent("\n"..table.concat(def, "\n")).."\nend)()" end)
 quote = op(function(l)
-  if type(l) == "string" then return "[["..l.."]]"
+  if type(l) == "string" then return escape(l)
   elseif type(l) == "number" then return tostring(l)
-  elseif getmt(l) == sym_mt then return "sym(\""..tostring(l).."\")"
+  elseif getmt(l) == sym_mt then return  "sym(\""..tostring(l).."\")"
   elseif getmt(l) == list_mt and l[1] == nil and l[2] == nil then return "nil"
   elseif getmt(l) == list_mt then return "list("..sep(l, quote)..")" end
 end)
-
--- write lua
+quasiquote = op(function(l)
+  if getmt(l) ~= list_mt then return quote(l) end
+  if getmt(l[1]) == sym_mt and tostr(l[1])=="quasiquote-eval" then 
+    return lua(l[2][1])
+  elseif l[1] == nil and l[2] == nil then return "nil" end
+  return "list("..sep(l, quasiquote)..")" end)
 if not input or not output then return end
-inf=io.open(input, "r") src = inf:read("*all") inf:close()
+inf=io.open(input, "r") src = inf:read("*all") inf:close() -- write lua
 of=io.open(output, "w") lf=io.open("l2l.lua", "r")
-  for i=1,94 do of:write(lf:read("*line").."\n") end lf:close()
+  for i=1,95 do of:write(lf:read("*line").."\n") end lf:close()
 of:write(compile(false, parse(src)).."\n") of:close()
--- test.lsp
+++ test.lsp
@@ -1,9 +1,12 @@
 (defun ! (n) 
   (cond ((eq n 0) 1)
         ((eq n 1) 1)
-        ('t (* n (! (- n 1))))))
+        (true (* n (! (- n 1))))))
 
 (print (! 100))
 
-(print (string.gsub "hello gibberish world" "gibberish " ""))
+(set hello-world "hello gibberish world")
+(print (string.gsub hello-world "gibberish " ""))
 
+(map print `(1 2 3)) 
+; prints all numbers only for lua 5.2, only 5.2 supports __ipairs override.
