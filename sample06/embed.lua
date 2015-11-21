def9e2edcd855633afe079ef227ac3fced8d2166
Eric Man <meric.au@gmail.com>
2015-11-22 01:34:46 +0800

    Added comments to sample06.

--- sample06/embed.lua
+++ sample06/embed.lua
@@ -1,8 +1,8 @@
--- This is an example to demonstrate embedding l2l in another lua program.
+-- This is an example to demonstrate embedding and sandboxing l2l in another
+-- lua program.
 -- Run using: lua sample06/embed.lua
 
 local core = require("core")
-
 
 local obj = core.read(core.reader.tofile([[
     (do
@@ -10,8 +10,17 @@ local obj = core.read(core.reader.tofile([[
         (print (map add_one '(1 2 3))))
 ]]))
 
-core.eval(obj, nil, {
-    add_one = function(x) return x + 1 end
-}, core.compiler.environment())
+local sandbox = core.compiler.environment()
 
+core.eval(obj, nil, {
+    -- insert function into sandboxed environment
+    add_one = function(x) return x + 1 end
+}, 
+-- the sandbox
+sandbox)
+
+-- prove it `set` did not modify _G.
 assert(_G.myvar == nil)
+
+-- peek inside sandbox
+print("(set myvar 4) => sandbox.myvar => ", sandbox.myvar)
