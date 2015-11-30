fb94a67bd480f2aa98fadf103d4b5a209c3b622d
Eric Man <meric.au@gmail.com>
2015-12-01 01:30:31 +0800

    Fixed listy test.

--- tests/init.lua
+++ tests/init.lua
@@ -1,5 +1,6 @@
 local _, compiler = require("l2l.compat"), require("l2l.compiler")
 local reader = require("l2l.reader")
+local show = require("l2l.itertools").show
 
 function dolispfile(filename)
   local f = io.open(filename, "r")
@@ -18,8 +19,8 @@ assert(mathy == -40, "Math failed! " .. mathy)
 
 local listy = dolispfile("tests/list.lsp")
 assert(listy[1] == 5, "List car failed! " .. listy[1])
--- assert(listy[2][1] == 9, "List cadr failed! " .. listy[2][1])
--- assert(listy[2][2] == 51, "List cddr failed! " .. listy[2][2])
+assert(listy[2][1] == 9, "List cadr failed! " .. show(listy[2][1]))
+assert(listy[2][2] == 51, "List cddr failed! " .. show(listy[2][2]))
 
 local fac, fib = dolispfile("tests/y.lsp")
 assert(fac(8) == 40320, "Factorial failed!")
