280ba63836ece8bc4409a045e56e3e6d9b5f7581
Eric Man <meric.au@gmail.com>
2015-12-01 15:37:20 +0800

    Re-added testing showing an improper list with a cons cell.

--- tests/init.lua
+++ tests/init.lua
@@ -1,6 +1,7 @@
 local _, compiler = require("l2l.compat"), require("l2l.compiler")
 local reader = require("l2l.reader")
-local show = require("l2l.itertools").show
+local itertools = require("l2l.itertools")
+local show, cons = itertools.show, itertools.cons
 
 function dolispfile(filename)
   local f = io.open(filename, "r")
@@ -21,9 +22,7 @@ local listy = dolispfile("tests/list.lsp")
 assert(listy[1] == 5, "List car failed! " .. tostring(listy[1]))
 assert(listy[2][1] == 9, "List cadr failed.")
 assert(listy[2][2] == 51, "List cddr failed.")
-
--- TODO: calling tostring(listy[2]) here on an improper list crashes it:
--- lua: ./l2l/itertools.lua:141: attempt to index local 'self' (a number value)
+assert(show(listy[2]) == show(itertools.cons(9, 51)))
 
 local fac, fib = unpack(dolispfile("tests/y.lsp"))
 assert(fac(8) == 40320, "Factorial failed!")
