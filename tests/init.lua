6c3b0fc664bd6ca14c136d32921a060938cb001f
Eric Man <meric.au@gmail.com>
2015-12-01 15:50:35 +0800

    Added loadfile, dofile interface

--- tests/init.lua
+++ tests/init.lua
@@ -1,29 +1,21 @@
 local _, compiler = require("l2l.compat"), require("l2l.compiler")
 local reader = require("l2l.reader")
 local itertools = require("l2l.itertools")
+local eval = require("l2l.eval")
 local show, cons = itertools.show, itertools.cons
 
-function dolispfile(filename)
-  local f = io.open(filename, "r")
-  assert(f, "File not found: " .. filename)
-  stream = reader.tofile(f:read("*all"))
-  f:close()
-  local ok, form = pcall(reader.read, stream, true)
-  assert(ok, "Compilation failed")
-  return compiler.eval(form)
-end
-
-local add = dolispfile("tests/add.lsp")
+local add = eval.dofile("tests/add.lsp")
 assert(add == 65, "Addition failed! ".. add)
-local mathy = dolispfile("tests/math.lsp")
+
+local mathy = eval.dofile("tests/math.lsp")
 assert(mathy == -40, "Math failed! " .. mathy)
 
-local listy = dolispfile("tests/list.lsp")
+local listy = eval.dofile("tests/list.lsp")
 assert(listy[1] == 5, "List car failed! " .. tostring(listy[1]))
 assert(listy[2][1] == 9, "List cadr failed.")
 assert(listy[2][2] == 51, "List cddr failed.")
 assert(show(listy[2]) == show(itertools.cons(9, 51)))
 
-local fac, fib = unpack(dolispfile("tests/y.lsp"))
+local fac, fib = unpack(eval.dofile("tests/y.lsp"))
 assert(fac(8) == 40320, "Factorial failed!")
 assert(fib(16) == 987, "Fibonacci failed!")
